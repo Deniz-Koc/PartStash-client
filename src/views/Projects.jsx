865178e4d3abf82376ce3ff53931e016b527a443
Deniz Koc <dkocusa@outlook.com>
2026-06-18 10:05:53 -0500

    Display projects list fetched from API

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,3 +1,26 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { getAllProjects } from "../services/projectService"
+
 export const Projects = () => {
-  return <h1>Projects Page</h1>
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    getAllProjects().then((data) => {
+      setProjects(data)
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1>Projects</h1>
+      {projects.map((project) => {
+        return (
+          <div key={project.id}>
+            <Link to={`/projects/${project.id}`}>{project.name}</Link>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
